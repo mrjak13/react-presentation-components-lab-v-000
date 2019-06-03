245d1affb968229ed8971a028a70af5850aaadb2
Justin Kenney <justinkenney13@gmail.com>
2019-06-03 21:53:02 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,7 +1,7 @@
 // Code SimplerComponent Here
 import React from 'react'
 
-const SimplerComponent = (props) => 
+const SimplerComponent = (props) =>
 
 	<div onClick={props.handleClick}>
 		<p>I am just happy</p>
